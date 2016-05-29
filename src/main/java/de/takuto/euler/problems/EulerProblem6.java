ae386e1caefb0aa136960c419978803948fce4c7
Lennart Rosam <hello@takuto.de>
2016-05-30 00:08:03 +0200

    Implement EulerProblem #6

---
+++ src/main/java/de/takuto/euler/problems/EulerProblem6.java
@@ -0,0 +1,34 @@
+package de.takuto.euler.problems;
+
+import de.takuto.euler.EulerProblem;
+
+/**
+ * The sum of the squares of the first ten natural numbers is,
+ *
+ * 12 + 22 + ... + 102 = 385
+ * The square of the sum of the first ten natural numbers is,
+ *
+ * (1 + 2 + ... + 10)2 = 552 = 3025
+ * Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+ *
+ * Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+ */
+public class EulerProblem6 implements EulerProblem {
+    public String solve() {
+        int sumOfSquares = 0;
+        int sumOfNumbers = 0;
+        for(int i = 1; i <= 100; i++) {
+            sumOfSquares += (i * i);
+            sumOfNumbers += i;
+        }
+
+        int squareSumOfSum = (sumOfNumbers * sumOfNumbers);
+        int difference =squareSumOfSum - sumOfSquares;
+
+        return "The difference between the sum of the squares of the first 100 natural numbers and the square of the sum is: " + squareSumOfSum + " - " + sumOfSquares + " = " + difference;
+    }
+
+    public int getNumber() {
+        return 6;
+    }
+}
