2ce4674af20a41bc27b3877a2e3ebbb3965871e9
Lennart Rosam <hello@takuto.de>
2016-05-29 23:48:08 +0200

    Implement EulerProblem #4

--- src/main/java/de/takuto/euler/problems/EulerProblem4.java
+++ src/main/java/de/takuto/euler/problems/EulerProblem4.java
@@ -13,7 +13,34 @@ public class EulerProblem4 implements EulerProblem {
 
 
     public String solve() {
-        return null;
+        int largestPalin = 0;
+
+        for(int x = 100; x <= 999; x++) {
+            for(int y = 100; y <= 999; y++){
+                int product = x * y;
+                if(isPalindrome(product) && largestPalin < product) {
+                    largestPalin = product;
+                }
+            }
+        }
+
+        return "The largest palindrome made from the product of two three digit numbers is: " + largestPalin;
+    }
+
+    private String reverse(String toReverse) {
+        char[] arr = toReverse.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for(int i = arr.length - 1; i >= 0; i--) {
+            sb.append(arr[i]);
+        }
+
+        return sb.toString();
+    }
+
+    private boolean isPalindrome(int number) {
+        String num = "" + number;
+        String palin = reverse(num);
+        return num.equals(palin);
     }
 
 
