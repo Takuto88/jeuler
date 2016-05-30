8d1430f7b4a211a4ae2f866f68b1e03532bcab42
Lennart Rosam <hello@takuto.de>
2016-05-30 22:39:45 +0200

    Implement EulerProblem #10

---
+++ src/main/java/de/takuto/euler/problems/EulerProblem10.java
@@ -0,0 +1,25 @@
+package de.takuto.euler.problems;
+
+import de.takuto.euler.EulerProblem;
+import de.takuto.euler.utils.MathUtil;
+
+/**
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ * Find the sum of all the primes below two million.
+ */
+public class EulerProblem10 implements EulerProblem {
+    private final int FIND_SUM_OF_PRIMES_BELOW_N = 2000000;
+    public String solve() {
+        long sumOfPrimes = 0;
+        for(int i = 1; i < FIND_SUM_OF_PRIMES_BELOW_N; i++) {
+            if(MathUtil.isPrime(i)){
+                sumOfPrimes += i;
+            }
+        }
+        return "The sum of all primes below " + FIND_SUM_OF_PRIMES_BELOW_N + " is: " + sumOfPrimes;
+    }
+
+    public int getNumber() {
+        return 10;
+    }
+}
