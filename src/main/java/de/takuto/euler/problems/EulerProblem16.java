12278069745fc213bbeff11585434232d47a6de7
Lennart Rosam <hello@takuto.de>
2016-12-29 14:42:27 +0100

    Implement EulerProblem #16

---
+++ src/main/java/de/takuto/euler/problems/EulerProblem16.java
@@ -0,0 +1,29 @@
+package de.takuto.euler.problems;
+
+import de.takuto.euler.EulerProblem;
+
+import java.math.BigInteger;
+
+/**
+ * 2 to the power of 15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 2 to the power of 1000?
+ */
+public class EulerProblem16 implements EulerProblem {
+
+    private static final int EXPONENT = 1000;
+
+    public String solve() {
+        String strRes = BigInteger.valueOf(2).pow(EXPONENT).toString();
+        int sum = 0;
+
+        for (char c : strRes.toCharArray()) {
+            sum += Integer.parseInt("" + c);
+        }
+
+        return "The sum of all digits of 2 to the power of " + EXPONENT + " is: " + sum;
+    }
+
+    public int getNumber() {
+        return 16;
+    }
+}
